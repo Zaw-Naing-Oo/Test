6aaf4d833298d2cd0c1674bd924cf4d47040eb24
ZawNaingOo0 <zawnaingoo.mhs@gmail.com>
2023-09-18 23:08:31 +0630

    addReq

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT  = process.env.PORT;
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
